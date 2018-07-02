4fd8c2c71edcca4f1b06e81d24e75fd2e07e5962
Una Bella <369152344@qq.com>
2018-07-02 15:47:16 +0800

    个人中心地址 客服
    closes #3 about #4

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,11 +19,13 @@ Page({
       icon: '',
       text: '我的地址',
       isunread: false,
+      method:'getAddress',
       // unreadNum: 2
     }, {
       icon: '',
       text: '联系客服',
       isunread: false,
+      connect:true
       // unreadNum: 1
     }, {
       icon: '',
@@ -32,19 +34,23 @@ Page({
     }],
     orderStatus:[
       {
-        url:'',
+        url: '',
+        methodStatus:'orderNopay',
         image:'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/Store/icon_unpaid@3x.png',
-        text:'代付款',
+        text:'待付款',
       },{
         url: '',
+        methodStatus: 'orderNosend',
         image: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/Store/icon_waiting_for_delivery@3x.png',
-        text: '代发货',
+        text: '待发货',
       },{
         url: '',
+        methodStatus: 'orderNoget',
         image: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/Store/icon_waiting_for_the_goods@3x.png',
         text: '待收货',
       }, {
         url: '',
+        methodStatus: 'orderFinish',
         image: 'http://llwell-wxapp.oss-cn-beijing.aliyuncs.com/Store/icon_finish@3x.png',
         text: '已完成',
       }
@@ -106,5 +112,58 @@ Page({
   onShareAppMessage: function () {
 
   },
-
+  // 待付款
+  orderNopay:function(){
+    console.log(1);
+    wx.navigateTo({
+      url: '../aboutUs_s/index?id=1'
+    })
+  },
+  // 待发货
+  orderNosend: function () {
+    console.log(2);
+  },
+  // 待收货
+  orderNoget: function () {
+    console.log(3);
+  },
+  // 待完成
+  orderFinish: function () {
+    console.log(4);
+  },
+  getAddress: function () {
+    var that = this;
+    wx.getSetting({
+      success(res) {
+        // console.log('成功输出')
+        if (!res.authSetting['scope.address']) {
+          wx.authorize({
+            scope: 'scope.address',
+            success() {
+              wx.chooseAddress({
+                success: function (res) {
+                  that.setData({
+                    inputPerson: res.userName,
+                    inputAddress: res.provinceName + res.cityName + res.countyName + res.detailInfo,
+                    inputPhone: res.telNumber,
+                  });
+                }
+              })
+            }
+          })
+        } else {
+          // console.log('失败输出')
+          wx.chooseAddress({
+            success: function (res) {
+              that.setData({
+                inputPerson: res.userName,
+                inputAddress: res.provinceName + res.cityName + res.countyName + res.detailInfo,
+                inputPhone: res.telNumber,
+              });
+            }
+          })
+        }
+      }
+    })
+  },
 })
